bd92175bea1b67aed5a075fb6847e4e627bac824
Pete Lawson <geekypete@gmail.com>
2022-11-28 21:29:14 -0500

    Added nested rectangles

--- p5-hello-world/sketch.js
+++ p5-hello-world/sketch.js
@@ -1,4 +1,5 @@
 let x = 1;
+let angle = 0;
 function setup() {
   createCanvas(800, 800);
   angleMode(DEGREES);
@@ -6,9 +7,19 @@ function setup() {
 
 function draw() {
   background(color("#034159"));
-  rect(30+x, 20+x, 55, 55);
-  noFill(); 
+  translate(mouseX, mouseY);
+  rotate(angle);
+  rectMode(CENTER);
+  rect(0, 0, 100, 100);
+  rect(0, 0, 90, 90);
+  rect(0, 0, 80, 80);
+  rect(0, 0, 70, 70);
+  rect(0, 0, 60, 60);
+  rect(0, 0, 50, 50);
+  rect(0, 0, 40, 40);
+  rect(0, 0, 30, 30);
+  noFill();
   stroke(color("#0CF25D"))
   x = x + 1;
-  rotate(PI/10);
+  angle = angle + 1;
 }
